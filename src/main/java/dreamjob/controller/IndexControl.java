c506274f63850d3094548e85cb5c80b02d849a62
VladislavCat <kshift84@gmail.com>
2022-09-19 17:02:52 +0300

    1. Что такое клиент - серверное приложение. [#504837]

---
+++ src/main/java/dreamjob/controller/IndexControl.java
@@ -0,0 +1,12 @@
+package dreamjob.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexControl {
+
+    @GetMapping("/index")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
